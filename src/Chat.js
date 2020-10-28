e5144faf7a11b2d87fab0fb858a341b9df38688f
Neilketchum <daipayanh@gmail.com>
2020-10-28 12:24:33 +0530

    Completed input Messages

--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,78 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import ChatHeader from './ChatHeader'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import GifIcon from '@material-ui/icons/Gif';
 const useStyles = makeStyles((theme) => ({
-   
-  }));
+    chat: {
+        display: "flex",
+        flexDirection: "column",
+        flex: "0.75",
+        width: "100%",
+        backgroundColor: "#3c3a3f",
+    },
+
+    chat__messages: {
+        flex:'1'
+    },
+    chat__input: {
+        color:"lightgray",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"space-between",
+        padding:"15px",
+        borderRadius:"15px",
+        margin:"20px",
+        borderTop:"1px solid gray",
+        backgroundColor:"#474b53",
+        "& form":{
+            flex:1,
+            "& input":{
+                padding:"15px",
+                background:"transparent",
+                border:"none",
+                outlineWidth:0,
+                color:"white",
+                fontSize:"large",
+                width:"100%"
+            }
+        }
+
+    },
+    chat__inputIcons:{
+        ".MuiSvgIcon-root":{
+            padding:"5px"
+        }
+
+    },
+}));
 export default function Chat() {
     const classes = useStyles();
     return (
-        <div>
-            <h2>I am Chat</h2>
+        <div className={classes.chat}>
+
+            <ChatHeader />
+
+
+
+            <div className={classes.chat__messages}>
+                VHat Messages
+            </div>
+            <div className= {classes.chat__input}>
+                <AddCircleOutlineIcon fontSize="large" />
+                <form>
+                    
+                    <input type="text" placeholder="Message " name="" id="" />
+                   
+                </form>
+                <div className={classes.chat__inputIcons}>
+                    <EmojiEmotionsIcon/>
+                    <GifIcon/>
+                    <CardGiftcardIcon/>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/ChatHeader.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PeopleIcon from '@material-ui/icons/People';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import HelpIcon from '@material-ui/icons/Help';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+import { flexbox } from '@material-ui/system';
+const useStyles = makeStyles((theme) => ({
+   ChatHeader:{
+        display:"flex",
+        justifyContent:"space-between",
+        alignItems:"center",
+        color:"gray",
+        padding:"10px",
+        width:"100%"
+   },
+   ChatHeader__right:{
+       display:"flex",
+       alignItems:"center",
+       flex:"0.5",
+       justifyContent:"space-between",
+       "& .MuiSvgIcon-root":{
+           padding:"5px",
+           cursor:"pointer",
+           fontSize:"1.7rem"
+       },
+       "& .MuiSvgIcon-root :hover":{
+        color:"white"
+        },
+
+
+   },
+   ChatHeader__left:{
+        "& h3":{
+            display:"flex",
+            alignItems:"center",
+            // color:"white"
+        }
+   },
+   ChatHeader__hash:{
+        color:"gray",
+        fontSize:"30px",
+        padding:"10px"
+   },
+   ChatHeader__search:{
+        display:"flex",
+        alignItems:"center",
+        color:"gray",
+        backgroundColor:"#2f3135",
+        borderRadius:"3px",
+        padding:"3px",
+        "& input":{
+            outlineWidth:"0",
+            background:"transparent",
+            color:"white",
+            border:"none"
+        }
+   }
+
+  }));
+export default function ChatHeader() {
+    const classes = useStyles();
+    return (
+        <div className = {classes.ChatHeader}>
+            <div className = {classes.ChatHeader__left}>
+                <h3>
+                    <span className = {classes.ChatHeader__hash}>
+                        #
+                    </span>
+                    Test Channel Name
+                </h3>
+            </div>
+            <div className = {classes.ChatHeader__right}>
+                <NotificationsIcon/>
+                <LocationOnIcon/>
+                <PeopleIcon/>
+                <div className = {classes.ChatHeader__search}>
+                    <input placeholder = "Search "></input>
+                    <SearchRoundedIcon/>
+                </div>
+                <SendRoundedIcon/>
+                <HelpIcon/>
+            </div>
+        </div>
+    )
+}
